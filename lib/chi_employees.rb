d9131293fe4a0ddd22e1a3f8689f1f4f34ac21e8
Jay Wengrow <jaywngrw@gmail.com>
2014-11-20 20:26:38 -0600

    added where method

--- lib/chi_employees.rb
+++ lib/chi_employees.rb
@@ -3,8 +3,21 @@ require "chi_employees/employee"
 require 'unirest'
 
 module ChiEmployees
+  BASE_URL = "http://data.cityofchicago.org/resource/xzkq-xp2w.json"
+
   def self.all
-    employee_hashes = Unirest.get("http://data.cityofchicago.org/resource/xzkq-xp2w.json").body
+    employee_hashes = Unirest.get(BASE_URL).body
+    generate_employees(employee_hashes)
+  end
+
+  def self.where(hash)
+    url = BASE_URL
+    url += "?#{hash.keys.first}=#{hash.values.first}"
+    employee_hashes = Unirest.get(url).body
+    generate_employees(employee_hashes)
+  end
+
+  def self.generate_employees(employee_hashes)
     employees = []
     employee_hashes.each do |hash|
       employees << Employee.new(hash)
