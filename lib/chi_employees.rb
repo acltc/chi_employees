95f00dddf48e1a2396d37d0a24b816b5d96aebfa
Jay Wengrow <jaywngrw@gmail.com>
2014-11-20 20:11:08 -0600

    added employee

--- lib/chi_employees.rb
+++ lib/chi_employees.rb
@@ -1,7 +1,14 @@
 require "chi_employees/version"
+require "chi_employees/employee"
+require 'unirest'
 
 module ChiEmployees
   def self.all
-    puts "Hi, there!"
+    employee_hashes = Unirest.get("http://data.cityofchicago.org/resource/xzkq-xp2w.json").body
+    employees = []
+    employee_hashes.each do |hash|
+      employees << Employee.new(hash)
+    end
+    employees
   end
 end
---
+++ lib/chi_employees/employee.rb
@@ -0,0 +1,12 @@
+module ChiEmployees
+  class Employee
+    attr_accessor :job_titles, :department, :name, :employee_annual_salary
+
+    def initialize(hash)
+      @job_titles = hash["job_titles"]
+      @department = hash["department"]
+      @name = hash["name"]
+      @employee_annual_salary = hash["employee_annual_salary"]
+    end
+  end
+end
